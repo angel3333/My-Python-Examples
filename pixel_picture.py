193e3df72893bd48bd06468839e00bcb2189e2d9
angel3333 <angel3.85bg@gmail.com>
2022-02-07 02:41:55 +0200

    example using fill with pixels

---
+++ pixel_picture.py
@@ -0,0 +1,18 @@
+picture = [
+  [1,1,0,1,0,1,1],
+  [1,0,0,0,0,0,1],
+  [0,1,0,0,0,1,0],
+  [0,0,1,0,1,0,0],
+  [0,0,0,1,0,0,0],
+  
+]
+
+fill = '@'
+empty = ' '
+for row in picture:
+  for pixel in row:
+    if (pixel):
+      print(fill, end =" ")
+    else:
+      print(empty, end =" ")
+  print('')
