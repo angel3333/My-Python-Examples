6e05d90d437e707a69eee33cb73b224fbd9b5ebe
angel3333 <angel3.85bg@gmail.com>
2022-03-06 23:26:08 +0200

    Parameters and Arguments example

--- Tesla.py
+++ Tesla.py
@@ -2,7 +2,9 @@ def checkDriverAge(age=0):
     if int(age) < 18:
         print("Sorry, you are too young to drive this car. Powering off 😟")
     elif int(age) > 18:
-        print("Powering On. Enjoy the ride! 🏎️");
+        print("Powering On. Enjoy the ride! 🏎️")
     elif int(age) == 18:
         print("Congratulations on your first year of driving. Enjoy the ride! 🚀")
+
+
 checkDriverAge()
