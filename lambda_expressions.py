b6135d93162e7e9afcaed29b3f7aa286c3928f97
angel3333 <angel3.85bg@gmail.com>
2022-03-13 05:09:25 +0200

    Lamda Expressions Example

--- lambda_expressions.py
+++ lambda_expressions.py
@@ -1,4 +1,6 @@
-#Lambda Expressions Example
+# Lambda Expressions Example
+
+# Lambda function inside the filter()
 student_name = ['Marie', 'Bobi', 'John', 'Andy',
                 'Sally', 'Anthony', 'Willy', 'Aaron', 'Ross']
 
@@ -7,6 +9,7 @@ result = list(filter(lambda x: (x[0] == 'A'), student_name))
 print(result)
 
 
+# Match the username with their email using zip()
 username = ['marie_coyman', 'bobi_garcia', 'john_smith', 'andy_tartakowsky',
             'sally_simon', 'anthony_hopkins', 'willy_deniro', 'aaron_jason', 'ross_kane']
 email = ['marie_coyman@mail.com', 'bobi_garcia@mail.com', 'john_smith@mail.com', 'andy_tartakowsky@mail.com',
@@ -15,14 +18,18 @@ email = ['marie_coyman@mail.com', 'bobi_garcia@mail.com', 'john_smith@mail.com',
 print(list(zip(username, email)))
 
 
+# Square using Lambda function
 print(list(map(lambda num: num**2, [1, 2, 3])))
 
+
+# Sort the numbers in tuples by having in mind second index
 a = [(0, 2), (5, 2), (9, 9), (10, -1)]
 a.sort(key=lambda x: x[1])
 
 print(a)
 
 
+# Find duplicates using Lambda function
 some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
 duplicates = list(set([x for x in some_list if some_list.count(x) > 1]))
 print(duplicates)
