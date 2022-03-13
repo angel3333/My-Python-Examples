8eff8dd65ad3b5c2f749b54b92258d193f755080
angel3333 <angel3.85bg@gmail.com>
2022-03-13 05:13:38 +0200

    Lamda Expressions Example

--- lambda_expressions.py
+++ lambda_expressions.py
@@ -22,7 +22,7 @@ print(list(zip(username, email)))
 print(list(map(lambda num: num**2, [1, 2, 3])))
 
 
-# Sort the numbers in tuples by having in mind second index
+# Sort the tuples by having in mind second index
 a = [(0, 2), (5, 2), (9, 9), (10, -1)]
 a.sort(key=lambda x: x[1])
 
