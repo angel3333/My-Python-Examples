1b3e149c79dc68ce1dc2a73c7d3e3c574924d82b
angel3333 <angel3.85bg@gmail.com>
2022-05-06 04:56:29 +0300

    Example for sys Module

--- 1.py
+++ 1.py
@@ -5,7 +5,7 @@ answer = randint(int(sys.argv[1]), int(sys.argv[2]))
 
 while True:
     try:
-        guess = int(input(f'guess a number {sys.argv[1]}, {sys.argv[2]}:  '))
+        guess = int(input(f'guess a number {sys.argv[1]}~{sys.argv[2]}:  '))
         if 0 < guess < 11:
             if guess == answer:
                 print('you are a genius!')
