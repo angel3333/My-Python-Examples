03223f824d0eaad1965eb4693937afa69ad505e2
angel3333 <angel3.85bg@gmail.com>
2022-05-12 13:15:01 +0300

    edit File I/O Example

--- file_IO_Translator.py
+++ file_IO_Translator.py
@@ -5,6 +5,7 @@ try:
     with open('./test.txt', mode='r') as my_file:
         text = my_file.read()
         translation = translator.translate(text)
-        print(translation)
+        with open('./test-ja.txt', mode='w') as my_file2:
+            my_file2.write(translation)
 except FileNotFoundError as e:
-        print('check your file path!')
+    print('check your file path!')
