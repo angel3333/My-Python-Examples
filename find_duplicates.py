30ffc6494e44bd155fd7b23b6e44175a96dee433
Melek Sabit <32045473+meleksabit@users.noreply.github.com>
2024-11-30 00:36:43 +0200

    fix: address suggestions from SonarCloud (#55)
    * add devsecops_pipeline.py
    
    * add devsecops-pipeline workflow
    
    * replace sonarqube with bandit
    
    * edit cron job
    
    * add comment for the cron job
    
    * add Git Guardian workflow
    
    * edit Git Guardian workflow
    
    * edit Git Guardian workflow
    
    * edit Git Guardian workflow
    
    * edit Git Guardian workflow
    
    * edit Git Guardian workflow
    
    * edit Git Guardian workflow
    
    * edit Git Guardian workflow
    
    * edit Git Guardian workflow
    
    * edit Git Guardian workflow
    
    * edit README file
    
    * edit DevSecOps pipeline
    
    * edit release badge
    
    * add .gitignore file
    
    * add PR Title Linter
    
    * add status badge for PR Linter
    
    * add SonarCloud implementation
    
    * edit sonar-project.properties
    
    * add SonarCloud badge
    
    * address suggestions from SonarCloud
    
    * add secrets module in strong_passgen_for_prod.py
    
    * edit strong_passgen_for_prod.py
    
    * edit strong_passgen_for_prod.py
    
    * edit strong_passgen_for_prod.py
    
    * edit strong_passgen_for_prod.py
    
    * edit strong_passgen_for_prod.py
    
    * edit strong_passgen_for_prod.py
    
    * edit strong_passgen_for_prod.py
    
    * edit strong_passgen_for_prod.py
    
    * edit crypto_passgen_for_prod.py script

--- find_duplicates.py
+++ find_duplicates.py
@@ -1,10 +1,7 @@
 # Shows the duplicate strings
 my_list = ['a', 'b', 'c', 'd', 'd', 'm', 'm', 'n', 'o', 'z', 'z']
-
 duplicates = []
 for value in my_list:
-    if my_list.count(value) > 1:
-        if value not in duplicates:
-            duplicates.append(value)
-
+    if my_list.count(value) > 1 and value not in duplicates:
+        duplicates.append(value)
 print(duplicates)
